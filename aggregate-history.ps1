7a9162a536d9f88b7a3033bac9cfadf88e417fef
Daniel Belcher <14930482+lotekdan@users.noreply.github.com>
2026-03-25 22:53:42 -0500

    fixing powershell for windows

--- aggregate-history.ps1
+++ aggregate-history.ps1
@@ -1,5 +1,10 @@
-go run ./cmd/main.go -d -7 > url_aggregator/out.json
+$currentDir = Get-Location
+$path = Join-Path $currentDir -ChildPath "url_aggregator\out.json"
+$output = go run .\cmd\main.go -d 7 -j
+[System.IO.File]::WriteAllText($path, $output)
+#gc .\url_aggregator\out.json  
 cd url_aggregator
-python url_aggregator.py -i out.json
+python .\url_aggregator.py -i .\out.json
 rm out.json
-python counts.py
+python .\counts.py
+cd ..
