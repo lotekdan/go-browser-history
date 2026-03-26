35866f8690cced0e55c6ec9e981928ae70db39fd
Daniel Belcher <14930482+lotekdan@users.noreply.github.com>
2026-03-25 23:03:01 -0500

    formating for easier readability

--- aggregate-history.ps1
+++ aggregate-history.ps1
@@ -1,10 +1,18 @@
+#Setting location and write output for history
 $currentDir = Get-Location
 $path = Join-Path $currentDir -ChildPath "url_aggregator\out.json"
 $output = go run .\cmd\main.go -d 7 -j
 [System.IO.File]::WriteAllText($path, $output)
-#gc .\url_aggregator\out.json  
-cd url_aggregator
+ 
+Set-Location url_aggregator
+
+#URL Aggregation of the history into 6 record sets:
+# Domains only 1, 5, 15, minute intervals
+# URL Path (no args) 1, 5, 15 minute intervals
 python .\url_aggregator.py -i .\out.json
-rm out.json
+Remove-Item out.json
+
+#Output record counts for the aggregated jsons
 python .\counts.py
-cd ..
+
+Set-Location ..
