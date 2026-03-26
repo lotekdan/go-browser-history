cc0e3ec368034e663e2fd83f08350c30aa153d8c
Daniel Belcher <14930482+lotekdan@users.noreply.github.com>
2026-03-25 23:07:28 -0500

    comments for readability

--- aggregate-history.ps1
+++ aggregate-history.ps1
@@ -7,7 +7,7 @@ $output = go run .\cmd\main.go -d 7 -j
 Set-Location url_aggregator
 
 #URL Aggregation of the history into 6 record sets:
-# Domains only 1, 5, 15, minute intervals
+# Domains only 1, 5, 15 minute intervals
 # URL Path (no args) 1, 5, 15 minute intervals
 python .\url_aggregator.py -i .\out.json
 Remove-Item out.json
--- aggregate-history.sh
+++ aggregate-history.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#Get history and write to url_aggregator directory
 go run ./cmd/main.go -d 7 -j > url_aggregator/out.json
+
 cd url_aggregator
+
+#URL Aggregation of the history into 6 record sets:
+# Domains only 1, 5, 15 minute intervals
+# URL Path (no args) 1, 5, 15 minute intervals
 python url_aggregator.py -i out.json
 rm out.json
+
+#Output record counts for the aggregated jsons
 python counts.py
